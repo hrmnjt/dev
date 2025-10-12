f1124b0564be43daff73fb50ba908e42b6e6c1ae
hrmnjt <harman@hrmnjt.dev>
2025-10-12 23:42:02 +0400

    feat: hardcode colors and remove lua table instead

--- stow/wezterm/.config/wezterm/wezterm.lua
+++ stow/wezterm/.config/wezterm/wezterm.lua
@@ -2,20 +2,6 @@ local wezterm = require 'wezterm'
 
 local config = wezterm.config_builder()
 
--- Catppuccin Mocha color palette
--- used for tab bar formatting
-local catppuccin_mocha = {
-    text = "#cdd6f4",
-    subtext1 = "#bac2de",
-    subtext0 = "#a6adc8",
-    surface2 = "#585b70",
-    surface1 = "#45475a",
-    surface0 = "#313244",
-    base = "#1e1e2e",
-    mantle = "#181825",
-    crust = "#11111b",
-}
-
 -- setting default font
 config.font = wezterm.font("JetBrains Mono")
 config.font_size = 18
@@ -32,11 +18,11 @@ config.hide_tab_bar_if_only_one_tab = true
 
 config.colors = {
     tab_bar = {
-        background = catppuccin_mocha.crust,
+        background = "#11111b",
 
         active_tab = {
-            bg_color = catppuccin_mocha.surface2,
-            fg_color = catppuccin_mocha.text,
+            bg_color = "#585b70",
+            fg_color = "#cdd6f4",
             intensity = 'Normal',
             underline = 'None',
             italic = false,
@@ -44,24 +30,24 @@ config.colors = {
         },
 
         inactive_tab = {
-            bg_color = catppuccin_mocha.surface0,
-            fg_color = catppuccin_mocha.subtext0,
+            bg_color = "#313244",
+            fg_color = "#a6adc8",
         },
 
         inactive_tab_hover = {
-            bg_color = catppuccin_mocha.surface1,
-            fg_color = catppuccin_mocha.text,
+            bg_color = "#313244",
+            fg_color = "#a6adc8",
             italic = true,
         },
 
         new_tab = {
-            bg_color = catppuccin_mocha.base,
-            fg_color = catppuccin_mocha.text,
+            bg_color = "#1e1e2e",
+            fg_color = "#cdd6f4",
         },
 
         new_tab_hover = {
-            bg_color = catppuccin_mocha.mantle,
-            fg_color = catppuccin_mocha.text,
+            bg_color = "#1e1e2e",
+            fg_color = "#cdd6f4",
             italic = true,
         },
     },
