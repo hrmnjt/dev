03d4ff1a1e36bba9b0f9d33451a8f8636a9ed0b1
hrmnjt <harman@hrmnjt.dev>
2025-12-01 22:15:39 +0400

    feat: added amp plugin to nvim

--- stow/nvim/.config/nvim/init.lua
+++ stow/nvim/.config/nvim/init.lua
@@ -974,6 +974,13 @@ require('lazy').setup({
     'numToStr/Comment.nvim',
   },
 
+  {
+    'sourcegraph/amp.nvim',
+    branch = 'main',
+    lazy = false,
+    opts = { auto_start = true, log_level = 'info' },
+  },
+
   -- The following comments only work if you have downloaded the kickstart repo, not just copy pasted the
   -- init.lua. If you want these files, they are in the repository, so you can just download them and
   -- place them in the correct locations.
