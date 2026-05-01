f7e58d3c307fc2b6a087d277650b6c35a961d1e7
hrmnjt <harman@hrmnjt.dev>
2026-05-02 00:58:40 +0530

    pi: replace git-clean filter with settings template

--- pi/.pi/agent/settings-clean.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-import json, sys
-try:
-    data = json.load(sys.stdin)
-    data.pop("lastChangelogVersion", None)
-    json.dump(data, sys.stdout, indent=2)
-    sys.stdout.write("\n")
-except Exception:
-    sys.stdout.write(sys.stdin.read())
