d4cc9c544af1d7b7abe5e902808a086e49459956
hrmnjt <harman@hrmnjt.dev>
2026-05-24 21:37:08 +0000

    fix(nvim): remove quit from dashboard keybindings

--- nvim/.config/nvim/lua/plugins/explorer.lua
+++ nvim/.config/nvim/lua/plugins/explorer.lua
@@ -35,12 +35,11 @@ return {
                 ||----w |
                 ||     ||
           ]],
-          -- Slimmed-down key shortcuts (default has 8, we keep 4)
+          -- Slimmed-down key shortcuts (default has 8, we keep 3)
           keys = {
             { icon = " ", key = "f", desc = "Find File", action = ":lua Snacks.dashboard.pick('files')" },
             { icon = " ", key = "n", desc = "New File", action = ":ene | startinsert" },
             { icon = " ", key = "g", desc = "Find Text", action = ":lua Snacks.dashboard.pick('live_grep')" },
-            { icon = " ", key = "q", desc = "Quit", action = ":qa" },
           },
         },
         sections = {
