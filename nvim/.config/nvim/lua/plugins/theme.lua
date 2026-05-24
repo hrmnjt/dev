d5b6d00345ef2a22221678ffb7b74552c4ca8f0e
hrmnjt <harman@hrmnjt.dev>
2026-05-24 21:00:22 +0000

    revert(nvim): keep built-in colorschemes, remove disable lines

--- nvim/.config/nvim/lua/plugins/theme.lua
+++ nvim/.config/nvim/lua/plugins/theme.lua
@@ -1,5 +1,4 @@
 -- Gruvbox theme (overrides LazyVim default)
--- Disable unused colorschemes that LazyVim includes by default
 return {
   {
     "ellisonleao/gruvbox.nvim",
@@ -12,6 +11,4 @@ return {
       colorscheme = "gruvbox",
     },
   },
-  { "catppuccin", enabled = false },
-  { "tokyonight.nvim", enabled = false },
 }
