808f9ec824319e55c4f8318d8edf8540e5a14bea
hrmnjt <harman@hrmnjt.dev>
2026-06-03 08:07:06 +0000

    Enable terminal and Neovim transparency

--- nvim/.config/nvim/lua/plugins/theme.lua
+++ nvim/.config/nvim/lua/plugins/theme.lua
@@ -3,7 +3,10 @@ return {
   {
     "ellisonleao/gruvbox.nvim",
     priority = 1000,
-    opts = {},
+    opts = {
+      -- Let Ghostty's transparent background show through Neovim.
+      transparent_mode = true,
+    },
   },
   {
     "LazyVim/LazyVim",
